6bd84522a11e2f3be586174f8e950277877fc000
Oscar Rieken <oscar_rieken@homedepot.com>
2015-12-13 18:47:52 -0500

    day one doge wow such script :dog2:

---
+++ day-1/wow.js
@@ -0,0 +1,30 @@
+var wow = [
+    "\n",
+    "                           W0W  ",
+    "                Y.                      _",
+    "                YiL                   .```.      ",
+    "                Yii;                .; .;;`.",
+    "                YY;ii._           .;`.;;;; :    ",
+    "                iiYYYYYYiiiii;;;;i` ;;::;;;;    ",
+    "            _.;YYYYYYiiiiiiYYYii  .;;.   ;;; ",
+    "         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;    ",
+    "       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..   ",
+    "      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.",
+    "      Y$MM$:   :YYYYYY$!````4YYYYYiiiYYYYiiYY.   ",
+    "   `. :MM$$b.,dYY$$Yii` :'   :YYYYllYiiYYYiYY            ",
+    "_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY",
+    ".,._ $b`P`     `4$$$$$iiiiiiii$$$$YY$$$$$$YiY;",
+    "   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL     ",
+    "    ``;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:  ",
+    "    ;$P$;;: ;;;;i$y$`!Y$$$b;$$$Y$YY$$YYYiiiYYiYY ",
+    "    $Fi$$ .. ``:iii.`-`:YYYYY$$YY$$$$$YYYiiYiYYY    ",
+    "    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:        ",
+    "     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY. ",
+    "      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY",
+    "      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY      ",
+    "     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'","\n",
+    "                       5UCH N0D3",
+    "\n"
+].join("\n");
+
+console.log(wow);
